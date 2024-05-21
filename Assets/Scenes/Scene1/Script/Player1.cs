b8890fe2c075a465fc7e27d9349a7ca881a1a56e
manxuan8231 <lydangxuanman05@gmail.com>
2024-05-21 13:43:46 +0700

    nmh

--- Assets/Scenes/Scene1/Script/Player1.cs
+++ Assets/Scenes/Scene1/Script/Player1.cs
@@ -7,7 +7,7 @@ public class Player1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //hello wo
     }
 
     // Update is called once per frame
